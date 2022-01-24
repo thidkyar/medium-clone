6fd8d674558cc33ad636222049888d78b2d37c40
thidkyar <thidkyar@gmail.com>
2022-01-23 21:16:50 -0500

    build header component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,15 @@
-import Head from 'next/head'
+import Head from "next/head";
+import Header from "../components/Header";
 
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen py-2">
+    <div className="">
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     <h1>
-       Medium-ish
-     </h1>
+      <Header />
     </div>
-  )
+  );
 }
