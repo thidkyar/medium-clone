a32fc913425cb0d0983694276fca2a06c5377f1b
thidkyar <thidkyar@gmail.com>
2022-01-25 22:23:42 -0500

    fix typescript assignment variable error

--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -23,7 +23,7 @@ function Post({ post }: Props) {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm<IFormInput>();
 
   const onSubmit: SubmitHandler<IFormInput> = async (data) => {
     fetch("/api/createComment", {
